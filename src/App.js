880fc0d29a3128ca6e461b535220b1619be32a1a
Smz1n <rogerio.brazilian@gmail.com>
2022-11-01 20:29:22 -0300

    rere

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  // useEffect(() => {
+  //   fetch('/books')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log('data', data)
+  //       setBooks(data)
+  //     })
+
+  // }, [])
+
+  useEffect(() => {
+    axios.get('/books')
+      .then(response => {
+        setBooks(response.data)
+        console.log(response);
+      })
+  }, [])
+   
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {JSON.stringify(books)}
         <a
           className="App-link"
           href="https://reactjs.org"
